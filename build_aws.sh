12bdcf7ad1314707c84067f1a28182143ba8fd57
gmanipon <gmanipon@jpl.nasa.gov>
2018-04-16 08:34:19 -0700

    handle multiple AWS profiles

--- build_aws.sh
+++ build_aws.sh
@@ -7,9 +7,9 @@ source_ami=$1
 subnet_id=$2
 
 for i in `echo mozart metrics grq factotum autoscale cont_int`; do
-  packer build -var "access_key=$(grep aws_access_key_id $HOME/.aws/credentials | awk '{print $3}')" \
-               -var "secret_key=$(grep aws_secret_access_key $HOME/.aws/credentials | awk '{print $3}')" \
-               -var "region=$(grep aws_secret_access_key $HOME/.aws/config | awk '{print $3}')" \
+  packer build -var "access_key=$(grep aws_access_key_id $HOME/.aws/credentials | head -1 | awk '{print $3}')" \
+               -var "secret_key=$(grep aws_secret_access_key $HOME/.aws/credentials | head -1 | awk '{print $3}')" \
+               -var "region=$(grep aws_secret_access_key $HOME/.aws/config | head -1 | awk '{print $3}')" \
                -var "ami_name=centos-7-x86_64-${i}-${DATE}" \
                -var "source_ami=${source_ami}" \
                -var "volume_size=20" \

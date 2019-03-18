83797cad71163ecb048a6ba8afe472fb6d6f5ce9
gmanipon <gmanipon@jpl.nasa.gov>
2019-03-18 14:00:44 -0700

    initial import of python3 AMI builders

---
+++ build_aws-python3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PACKER_LOG=1
+DATE=$(date '+%Y%m%d')
+
+source_ami=$1
+subnet_id=$2
+
+#for i in `echo mozart metrics grq factotum autoscale cont_int`; do
+for i in `echo mozart`; do
+  packer build -var "access_key=$(grep aws_access_key_id $HOME/.aws/credentials | head -1 | awk '{print $3}')" \
+               -var "secret_key=$(grep aws_secret_access_key $HOME/.aws/credentials | head -1 | awk '{print $3}')" \
+               -var "region=$(grep region $HOME/.aws/config | head -1 | awk '{print $3}')" \
+               -var "ami_name=centos-7-x86_64-${i}-${DATE}" \
+               -var "source_ami=${source_ami}" \
+               -var "volume_size=20" \
+               -var "subnet_id=${subnet_id}" \
+               -var "hysds_component=${i}" \
+               hysds_aws-python3.json
+done

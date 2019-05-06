7a63eca3a48b1ea0ed31ac42b68303b3e80ff4b2
gmanipon <gmanipon@jpl.nasa.gov>
2019-05-06 16:26:53 -0700

    add AMI creation of generic hysds AMI

---
+++ build_aws_base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PACKER_LOG=1
+DATE=$(date '+%Y%m%d')
+
+source_ami=$1
+subnet_id=$2
+
+for i in `echo hysds_base`; do
+  packer build -var "access_key=$(grep aws_access_key_id $HOME/.aws/credentials | head -1 | awk '{print $3}')" \
+               -var "secret_key=$(grep aws_secret_access_key $HOME/.aws/credentials | head -1 | awk '{print $3}')" \
+               -var "region=$(grep region $HOME/.aws/config | head -1 | awk '{print $3}')" \
+               -var "ami_name=centos-7-x86_64-${i}-py3-${DATE}" \
+               -var "source_ami=${source_ami}" \
+               -var "volume_size=30" \
+               -var "subnet_id=${subnet_id}" \
+               -var "hysds_component=${i}" \
+               hysds_aws_base.json
+done
